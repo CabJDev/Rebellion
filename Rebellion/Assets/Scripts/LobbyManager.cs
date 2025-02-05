fae1fa2f7ddbb076f813a967f91ac725f1702e53
John <cabjdev@gmail.com>
2025-02-05 12:54:17 -0700

    Player management and main menu

--- Rebellion/Assets/Scripts/LobbyManager.cs
+++ Rebellion/Assets/Scripts/LobbyManager.cs
@@ -1,5 +1,6 @@
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LobbyManager : MonoBehaviour
 {
@@ -11,14 +12,81 @@ public class LobbyManager : MonoBehaviour
     private int testPlayers;
     [SerializeField]
     private int rebels;
-    [SerializeField]
     private int neutrals;
 
+    /*
+     * Alignment Lists
+     * 0 = null
+     * 1 = Loyalists
+     * 2 = Rebels
+     * 3 = Neutrals
+     */
+
+    /*
+     * Role Lists
+     * 0 = null
+     * 1 = Offensive
+     * 2 = Defensive
+     * 3 = Supportive
+     */
+
+    private void Start()
+    {
+        playerManager = PlayerManager.Instance;
+    }
+
+    // TODO: Set up actual lobby creation
     public void ButtonPressed()
     {
+        // Alignment selection
+        neutrals = Random.Range(0, 3);
+
         for (int i = 0; i < testPlayers; ++i)
         {
-            playerManager.AddPlayer("Player_" + i);
+            playerManager.RemovePlayer(i);
+            playerManager.AddPlayer("Player_" + (i + 1));
         }
+
+        int[] rebelIDs = new int[rebels];
+
+        for (int i = 0; i < rebels; ++i)
+        {
+            int id = Random.Range(1, testPlayers);
+            while (rebelIDs.Contains(id)) id = Random.Range(1, testPlayers);
+            rebelIDs[i] = id;
+            playerManager.SetAlignment(id, 2);
+
+            if (i == 0)
+                playerManager.SetRole(id, 1);
+            else
+                playerManager.SetRole(id, Random.Range(1, 4));
+        }
+
+        for (int i = 0; i < neutrals; ++i)
+        {
+            int id = Random.Range(1, testPlayers);
+
+            while (rebelIDs.Contains(id))
+                id = Random.Range(1, testPlayers);
+
+            playerManager.SetAlignment(id, 3);
+
+            if (i == 0)
+                playerManager.SetRole(id, 1);
+            else
+                playerManager.SetRole(id, Random.Range(1, 4));
+        }
+
+        for (int i = 0; i < testPlayers; ++i)
+        {
+            if (playerManager.IsPlayer(i) && !playerManager.IsAligned(i))
+            {
+                playerManager.SetAlignment(i, 1);
+                playerManager.SetRole(i, Random.Range(1, 4));
+            }
+        }
+
+        playerManager.PrintPlayers();
+        SceneManager.LoadScene("Gameplay");
     }
 }
--- Rebellion/Assets/Scripts/PlayerManager.cs
+++ Rebellion/Assets/Scripts/PlayerManager.cs
@@ -15,10 +15,25 @@ struct Player
         this.role = role;
     }
 }
+
 public class PlayerManager : MonoBehaviour
 {
+    public static PlayerManager Instance;
+
     Player[] players;
     Player nullPlayer;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     private void Start()
     {
@@ -31,15 +46,92 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    public void AddPlayer(string playerName)
+    public bool AddPlayer(string playerName)
     {
-        int id = players.Length;
 
-        players[id] = new Player(id, playerName, 0, 0);
+        for (int i = 0; i < players.Length; ++i)
+        {
+            if (players[i].id == 0)
+            {
+                players[i] = new Player(i + 1, playerName, 0, 0);
+                Debug.Log("Added player at " + (i + 1));
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    public void RemovePlayer(int id)
+    public void RemovePlayer(int id) { players[id] = nullPlayer; }
+
+    public void SetAlignment(int id, int alignment) { players[id].alignment = alignment; }
+
+    public void SetRole(int id, int role) { players[id].role = role; }
+
+    public bool IsPlayer(int id)
     {
-        players[id] = nullPlayer;
+        if (players[id].id == 0) return false;
+        else return true;
+    }
+
+    public bool IsAligned(int id)
+    {
+        if (players[id].alignment == 0) return false;
+        else return true;
+    }
+
+    public void PrintPlayers()
+    {
+        int rebelAmount = 0;
+        int neutralAmount = 0;
+        int loyalistAmount = 0;
+
+        for (int i = 0; i < players.Length; ++i)
+        {
+            if (players[i].id != 0)
+            {
+                string alignment = "";
+                string role = "";
+
+                switch (players[i].alignment)
+                {
+                    case 0:
+                        alignment = "null";
+                        break;
+                    case 1:
+                        alignment = "Loyalist";
+                        loyalistAmount++;
+                        break;
+                    case 2:
+                        alignment = "Rebel";
+                        rebelAmount++;
+                        break;
+                    case 3:
+                        alignment = "Neutral";
+                        neutralAmount++;
+                        break;
+                }
+
+                switch (players[i].role)
+                {
+                    case 0:
+                        role = "null";
+                        break;
+                    case 1:
+                        role = "Offensive";
+                        break;
+                    case 2:
+                        role = "Defensive";
+                        break;
+                    case 3:
+                        role = "Supportive";
+                        break;
+                }
+
+                Debug.Log(players[i].name + " | Alignment: " + alignment + " | Role: " + role );
+            }
+        }
+
+        Debug.Log("There are: " + loyalistAmount + " loyalists, " +  rebelAmount + " rebels and " + neutralAmount + " neutrals");
     }
 }
