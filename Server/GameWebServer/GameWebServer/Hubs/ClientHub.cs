29b708c2b731a73504696c9cf4b47fc77bf1196f
CabJDev <cabjdev@gmail.com>
2025-02-18 13:16:31 -0700

    Web Server

---
+++ Server/GameWebServer/GameWebServer/Hubs/ClientHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace GameWebServer.Hubs
+{
+    public class ClientHub : Hub
+    {
+        public async Task SendMessage(string name, string lobbyCode)
+        {
+            Console.WriteLine($"{name} joined lobby with code {lobbyCode}");
+        }
+    }
+}
---
+++ Server/GameWebServer/GameWebServer/wwwroot/js/clienthub.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/ClientHub").build();
+
+document.getElementById("submitButton").disabled = true;
+
+connection.start().then(function () {
+    document.getElementById("submitButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+})
+
+document.getElementById("submitButton").addEventListener("click", function (event) {
+    var name = document.getElementById("name").value;
+    var lobby = document.getElementById("lobbyCode").value;
+    connection.invoke("SendMessage", name, lobby).catch(function (err) {
+        return console.error(err.toString());
+    })
+
+    event.preventDefault();
+})
